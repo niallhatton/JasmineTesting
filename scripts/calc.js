280093af915b6485e15f1fd1170e27673430c9c7
niallhatton <niall@elh-it.com>
2020-11-08 20:16:28 +0000

    Typeof check for numbers or throw error

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,3 +1,6 @@
 function addition(numOne, numTwo){
-    return numOne + numTwo;
+    if (typeof(numOne) == "number" && typeof(numTwo) == "number") {
+        return numOne + numTwo;
+    }
+    return "Error!";
 }
