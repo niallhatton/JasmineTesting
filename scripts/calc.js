b4089204847a8cd45e173d4e04b2242ced9f929c
niallhatton <niall@elh-it.com>
2020-11-08 21:13:51 +0000

    Breaking the functions of the code to prove tests fail

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,7 +1,19 @@
-function addition(numOne, numTwo){
+Calculator = function() {
+    this.value = 0;
+};
+
+Calculator.prototype.add = function(number) {
+    if (typeof(number) == "number") {
+        this.value += number;
+    } else {
+        alert("Error!");
+    }
+};
+/* function addition(numOne, numTwo){
     if (typeof(numOne) == "number" && typeof(numTwo) == "number") {
         return numOne + numTwo;
     } else {
         alert("Error!");
     }
 }
+*/
