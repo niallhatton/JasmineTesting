84ddd7e59fc8e09b59d2d0b1cd9606baa3119c1e
niallhatton <niall@elh-it.com>
2020-11-08 21:21:04 +0000

    Callback function to create new calc object before each test

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,14 +1,26 @@
 describe("Calculator", function() {
-    describe("Addition tests", function(){
-        it("should be 42", function(){
-            expect(addition(20, 22)).toBe(42);
+
+    beforeEach(function() {
+       calc = new Calculator; 
+    });
+    var calc = new Calculator;
+
+    describe("Addition function", function() {
+        it("should return 42", function() {
+            calc.add(20);
+            calc.add(22);
+            expect(calc.value).toBe(42);
         });
-        it("should return 26", function(){
-            expect(addition(7, 19)).toBe(26);
+
+        it("should return 26", function() {
+            calc.add(7);
+            calc.add(19);
+            expect(calc.value).toBe(26);
         });
-        it("should return an error if we do not supply two numbers", function(){
+
+        it("should return an error if we don't supply two numbers", function() {
             spyOn(window, "alert");
-            addition("Hitchhikers", "Guide");
+            calc.add("Hitchhikers");
             expect(window.alert).toHaveBeenCalledWith("Error!");
         });
     });
